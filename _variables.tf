ab8ac7458ba147c95ce7a8f9d4c306437f46c7ba
Allan Denot <adenot@gmail.com>
2019-06-12 23:37:55 +1000

    Adding app autoscaling for ECS service with CPU tracking

--- _variables.tf
+++ _variables.tf
@@ -43,3 +43,33 @@ variable "alb_listener_https_arn" {}
 variable "alb_dns_name" {}
 
 variable "certificate_arn" {}
+
+variable "autoscaling_cpu" {
+  default     = false
+  description = "Enables autoscaling based on average CPU tracking"
+}
+
+variable "autoscaling_max" {
+  default     = 4
+  description = "Max number of containers to scale with autoscaling"
+}
+
+variable "autoscaling_min" {
+  default     = 1
+  description = "Min number of containers to scale with autoscaling"
+}
+
+variable "autoscaling_target_cpu" {
+  default     = 50
+  description = "Target average CPU percentage to track for autoscaling"
+}
+
+variable "autoscaling_scale_in_cooldown" {
+  default     = 300
+  description = "Cooldown in seconds to wait between scale in events"
+}
+
+variable "autoscaling_scale_out_cooldown" {
+  default     = 300
+  description = "Cooldown in seconds to wait between scale out events"
+}
--- appautoscaling.tf
+++ appautoscaling.tf
@@ -1,25 +1,34 @@
-# resource "aws_appautoscaling_target" "ecs_target" {
-#   max_capacity       = 4
-#   min_capacity       = 1
-#   resource_id        = "service/clusterName/serviceName"
-#   role_arn           = "${var.ecs_iam_role}"
-#   scalable_dimension = "ecs:service:DesiredCount"
-#   service_namespace  = "ecs"
-# }
-# resource "aws_appautoscaling_policy" "ecs_policy" {
-#   name               = "scale-down"
-#   policy_type        = "StepScaling"
-#   resource_id        = "${aws_appautoscaling_target.ecs_target.resource_id}"
-#   scalable_dimension = "${aws_appautoscaling_target.ecs_target.scalable_dimension}"
-#   service_namespace  = "${aws_appautoscaling_target.ecs_target.service_namespace}"
-#   step_scaling_policy_configuration {
-#     adjustment_type         = "ChangeInCapacity"
-#     cooldown                = 60
-#     metric_aggregation_type = "Maximum"
-#     step_adjustment {
-#       metric_interval_upper_bound = 0
-#       scaling_adjustment          = -1
-#     }
-#   }
-# }
+resource "aws_iam_service_linked_role" "autoscaling" {
+  aws_service_name = "autoscaling.amazonaws.com"
+  custom_suffix    = "${var.cluster_name}-${var.name}"
+}
 
+resource "aws_appautoscaling_target" "ecs" {
+  count              = "${var.autoscaling_cpu ? 1 : 0}"
+  max_capacity       = "${var.autoscaling_max}"
+  min_capacity       = "${var.autoscaling_min}"
+  resource_id        = "service/${var.cluster_name}/${aws_ecs_service.default.name}"
+  role_arn           = "${aws_iam_service_linked_role.autoscaling.arn}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "scale_cpu" {
+  count              = "${var.autoscaling_cpu ? 1 : 0}"
+  name               = "scale-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = "${aws_appautoscaling_target.ecs.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.ecs.scalable_dimension}"
+  service_namespace  = "${aws_appautoscaling_target.ecs.service_namespace}"
+
+  target_tracking_scaling_policy_configuration = {
+    target_value       = "${var.autoscaling_target_cpu}"
+    disable_scale_in   = false
+    scale_in_cooldown  = "${var.autoscaling_scale_in_cooldown}"
+    scale_out_cooldown = "${var.autoscaling_scale_out_cooldown}"
+
+    predefined_metric_specification = {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+  }
+}
