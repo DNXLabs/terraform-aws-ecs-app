fc1e2414ad47be9ad483ae8d7bc639959a2d785d
Allan Denot <adenot@gmail.com>
2019-09-25 21:44:18 +1000

    Testing external deployment

--- ecs-service.tf
+++ ecs-service.tf
@@ -2,22 +2,13 @@
 resource "aws_ecs_service" "default" {
   name                              = "${var.name}"
   cluster                           = "${var.cluster_name}"
-  task_definition                   = "${aws_ecs_task_definition.default.arn}"
   desired_count                     = 1
-  iam_role                          = "${var.service_role_arn}"
-  health_check_grace_period_seconds = "${var.service_health_check_grace_period_seconds}"
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.green.arn}"
-    container_name   = "${var.name}"
-    container_port   = "${var.container_port}"
-  }
 
   deployment_controller {
-    type = "CODE_DEPLOY"
+    type = "EXTERNAL"
   }
 
   lifecycle {
-    ignore_changes = ["load_balancer", "task_definition", "desired_count"]
+    ignore_changes = ["desired_count"]
   }
 }
