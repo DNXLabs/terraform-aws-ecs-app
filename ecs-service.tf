3702f299121b4f192d69da52e9c17092cea88d63
Allan Denot <adenot@gmail.com>
2019-12-18 11:33:49 +1100

    Tf12 language

--- ecs-service.tf
+++ ecs-service.tf
@@ -1,8 +1,8 @@
 
 resource "aws_ecs_service" "default" {
-  name                              = "${var.name}"
-  cluster                           = "${var.cluster_name}"
-  desired_count                     = 1
+  name          = var.name
+  cluster       = var.cluster_name
+  desired_count = 1
 
   deployment_controller {
     type = "EXTERNAL"
