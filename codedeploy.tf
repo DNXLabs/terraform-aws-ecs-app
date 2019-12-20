314412f581bddee94845fad02cd11354a11f2394
Helder Klemp <helderklemp@gmail.com>
2019-12-20 13:34:04 +1100

    add 5 minutos to route traffic

--- codedeploy.tf
+++ codedeploy.tf
@@ -17,12 +17,14 @@ resource "aws_codedeploy_deployment_group" "ecs" {
   blue_green_deployment_config {
     deployment_ready_option {
       action_on_timeout = "CONTINUE_DEPLOYMENT"
+      wait_time_in_minutes = 5
     }
 
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
       termination_wait_time_in_minutes = 0
     }
+
   }
 
   deployment_style {
