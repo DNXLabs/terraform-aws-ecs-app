03b437da24af59217d5d6a40123835b621a37d93
Helder Klemp <helderklemp@gmail.com>
2019-12-22 09:11:35 +1100

    adding STOP_DEPLOYMENT

--- codedeploy.tf
+++ codedeploy.tf
@@ -16,7 +16,7 @@ resource "aws_codedeploy_deployment_group" "ecs" {
 
   blue_green_deployment_config {
     deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
+      action_on_timeout = "STOP_DEPLOYMENT"
       wait_time_in_minutes = "${var.codedeploy_wait_time_for_cutover}"
     }
 
