ef0bd45fa4cf85362b512459fa547c9fe46c9fb4
Helder Klemp <helderklemp@gmail.com>
2019-12-22 08:58:11 +1100

    add codedeploy variables

--- codedeploy.tf
+++ codedeploy.tf
@@ -17,12 +17,12 @@ resource "aws_codedeploy_deployment_group" "ecs" {
   blue_green_deployment_config {
     deployment_ready_option {
       action_on_timeout = "CONTINUE_DEPLOYMENT"
-      wait_time_in_minutes = 5
+      wait_time_in_minutes = "${var.codedeploy_wait_time_for_cutover}"
     }
 
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 0
+      termination_wait_time_in_minutes = "${var.codedeploy_wait_time_for_termination}"
     }
 
   }
