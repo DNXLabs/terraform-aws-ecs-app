1cea60ee359260a01af99215e982203aa4e51846
Helder Klemp <helderklemp@gmail.com>
2019-12-26 09:08:50 +1100

    add 0 deployment  as default

--- codedeploy.tf
+++ codedeploy.tf
@@ -16,13 +16,13 @@ resource "aws_codedeploy_deployment_group" "ecs" {
 
   blue_green_deployment_config {
     deployment_ready_option {
-      action_on_timeout = "STOP_DEPLOYMENT"
-      wait_time_in_minutes = "${var.codedeploy_wait_time_for_cutover}"
+      action_on_timeout = var.codedeploy_wait_time_for_cutover == 0 ? "CONTINUE_DEPLOYMENT" : "STOP_DEPLOYMENT"
+      wait_time_in_minutes = var.codedeploy_wait_time_for_cutover
     }
 
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = "${var.codedeploy_wait_time_for_termination}"
+      termination_wait_time_in_minutes = var.codedeploy_wait_time_for_termination
     }
 
   }
