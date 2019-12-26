d2ad5d2a5cf8779d268b8e8e95d0f41fa36065af
Allan Denot <adenot@gmail.com>
2019-12-26 17:10:03 +1100

    Using depends_on to prevent ecs service creation with a target group that does not have a listener rule

--- ecs-service.tf
+++ ecs-service.tf
@@ -1,16 +1,16 @@
 
 resource "aws_ecs_service" "default" {
-  name                              = "${var.name}"
-  cluster                           = "${var.cluster_name}"
-  task_definition                   = "${aws_ecs_task_definition.default.arn}"
+  name                              = var.name
+  cluster                           = var.cluster_name
+  task_definition                   = aws_ecs_task_definition.default.arn
   desired_count                     = 1
-  iam_role                          = "${var.service_role_arn}"
-  health_check_grace_period_seconds = "${var.service_health_check_grace_period_seconds}"
+  iam_role                          = var.service_role_arn
+  health_check_grace_period_seconds = var.service_health_check_grace_period_seconds
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.green.arn}"
-    container_name   = "${var.name}"
-    container_port   = "${var.container_port}"
+    target_group_arn = aws_lb_target_group.green.arn
+    container_name   = var.name
+    container_port   = var.container_port
   }
 
   deployment_controller {
@@ -20,4 +20,9 @@ resource "aws_ecs_service" "default" {
   lifecycle {
     ignore_changes = ["load_balancer", "task_definition", "desired_count"]
   }
+  
+  depends_on = [
+    aws_lb_listener_rule.green,
+    aws_lb_listener_rule.blue
+  ]
 }
