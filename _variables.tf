4bbeab123cb619cf2d0456022e0acfc11a8f2f58
Arthur Diniz <arthurbdiniz@gmail.com>
2020-07-21 00:45:20 -0300

    Removes var name and log group from sub filter
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _variables.tf
+++ _variables.tf
@@ -235,23 +235,15 @@ variable "log_subscription_filter_enabled" {
   type = string
 }
 
-variable "log_subscription_filter_name" {
-  type = string
-}
-
 variable "log_subscription_filter_role_arn" {
   type = string
 }
 
-variable "log_subscription_filter_log_group_name" {
+variable "log_subscription_filter_destination_arn" {
   type = string
 }
 
 variable "log_subscription_filter_filter_pattern" {
   default = ""
-  type = string
-}
-
-variable "log_subscription_filter_destination_arn" {
   type = string
 }
--- cloudwatch-logs-subscription-filter.tf
+++ cloudwatch-logs-subscription-filter.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_subscription_filter" "log_subscription_filter" {
   count = var.log_subscription_filter_enabled ? 1 : 0
 
-  name            = var.log_subscription_filter_name
-  role_arn        = var.log_subscription_filter_role_arn
-  log_group_name  = var.log_subscription_filter_log_group_name
+  name            = "${var.name}_subscription_filter"
+  log_group_name  = aws_cloudwatch_log_group.default
   filter_pattern  = var.log_subscription_filter_filter_pattern
+  role_arn        = var.log_subscription_filter_role_arn
   destination_arn = var.log_subscription_filter_destination_arn
 }
