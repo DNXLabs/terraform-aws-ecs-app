fce9e6368da61213973a396c37e550a045cc7eb7
Marcus <marcus.nogueira@dnx.solutions>
2021-06-25 15:00:27 +1000

    Updating Variables

--- cloudwatch-alarms.tf
+++ cloudwatch-alarms.tf
@@ -54,7 +54,7 @@ resource "aws_cloudwatch_metric_alarm" "min_healthy_tasks" {
 }
 
 resource "aws_appautoscaling_policy" "scale_alb" {
-  count              =  var.autoscaling_cpu ? 1 : 0
+  count              =  var.cloudwatch_metric_alb_connections ? 1 : 0
   name               = "alb_scaling"
   policy_type        = "StepScaling"
   resource_id        = aws_appautoscaling_target.ecs[0].resource_id
@@ -77,7 +77,9 @@ resource "aws_appautoscaling_policy" "scale_alb" {
 
 
 resource "aws_cloudwatch_metric_alarm" "alb-connections" {
-  count = var.log_subscription_filter_enabled ? 1 : 0
+  count = var.cloudwatch_metric_alb_connections ? 1 : 0
+
+  
   alarm_name                = "${data.aws_iam_account_alias.current.account_alias}-ecs-${var.cluster_name}-${var.name}-alb-average-connections"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
