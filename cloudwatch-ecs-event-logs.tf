e9ecc80734a0268d0c225542a7aad083a4e5aa6b
Luiz Rocha <luiz.rocha@brighte.com.au>
2022-03-22 09:34:47 +1100

    Update log group name

--- cloudwatch-ecs-event-logs.tf
+++ cloudwatch-ecs-event-logs.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "ecs_events" {
-  name              = "ecs/events/${var.cluster_name}/${var.name}"
+  name              = "/ecs/events/${var.cluster_name}/${var.name}"
   retention_in_days = var.cloudwatch_logs_retention
   tags = {
     ExportToS3 = var.cloudwatch_logs_export
