ccdd72599983c1145dd16502f60c5ca69ecb1c45
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2024-02-06 18:06:21 +1100

    create roles

---
+++ iam-ecs-task.tf
@@ -0,0 +1,77 @@
+resource "aws_iam_role" "ecs_task" {
+  count = var.task_role_arn != null ? 0 : 1
+  name = "ecs-task-${var.cluster_name}-${var.name}-${data.aws_region.current.name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  count = var.task_role_arn != null ? 0 : 1
+  role       = aws_iam_role.ecs_task[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+  depends_on = [ aws_iam_role.ecs_task ]
+}
+
+resource "aws_iam_role_policy" "ssm_policy" {
+  count = var.task_role_arn != null ? 0 : 1
+  name = "ecs-ssm-policy"
+  role = aws_iam_role.ecs_task[0].name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetResourcePolicy",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "secretsmanager:ListSecretVersionIds"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameters"
+      ],
+      "Resource": [
+        "arn:aws:ssm:*:*:parameter/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+
+depends_on = [ aws_iam_role.ecs_task ]
+}
