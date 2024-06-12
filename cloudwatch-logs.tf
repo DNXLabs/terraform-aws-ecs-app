1f34e7f3decd8d0131c780ef8e3873e6f2466094
mvsnogueira-dnx <marcus.nogueira@brighte.com.au>
2024-06-12 14:45:41 +1000

    adding back ExportToS3 tags

--- cloudwatch-logs.tf
+++ cloudwatch-logs.tf
@@ -1,8 +1,12 @@
 resource "aws_cloudwatch_log_group" "default" {
   name              = "/ecs/${var.cluster_name}/${var.name}"
   retention_in_days = var.cloudwatch_logs_retention
-  tags              = var.tags
-
+  tags = merge(
+    var.tags,
+    {
+      ExportToS3 = var.cloudwatch_logs_export
+    }
+  )
 }
 
 
