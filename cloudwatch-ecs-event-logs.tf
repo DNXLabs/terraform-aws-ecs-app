e225731a422a2b513cb7b0a5ce714720a32584d4
Lucas de Macedo <122332519+lucasmacedot@users.noreply.github.com>
2025-04-30 17:01:02 -0400

    Remove cw events log policy

--- cloudwatch-ecs-event-logs.tf
+++ cloudwatch-ecs-event-logs.tf
@@ -35,28 +35,3 @@ resource "aws_cloudwatch_event_target" "ecs_events" {
   rule  = aws_cloudwatch_event_rule.ecs_events[0].name
   arn   = aws_cloudwatch_log_group.ecs_events[0].arn
 }
-
-data "aws_iam_policy_document" "ecs_events" {
-  count = var.cloudwatch_logs_create ? 1 : 0
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:PutLogEventsBatch",
-    ]
-
-    resources = ["${aws_cloudwatch_log_group.ecs_events[0].arn}:*"]
-
-    principals {
-      identifiers = ["events.amazonaws.com", "delivery.logs.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_resource_policy" "ecs_events" {
-  count           = var.cloudwatch_logs_create ? 1 : 0
-  policy_document = data.aws_iam_policy_document.ecs_events[0].json
-  policy_name     = "capture-ecs-events-${var.cluster_name}-${var.name}"
-
-}
